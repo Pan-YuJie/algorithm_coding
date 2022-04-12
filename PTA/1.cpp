cb47eb466132c0ea4a04d2b9847247219d793614
Pan_Yujie <646836760@qq.com>
2022-04-12 19:08:20 +0800

    master

---
+++ PTA/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "我的大大大大的";
+    cout << "我的大大大大的";
+    cout << "我的大大大大的";
+    cout << "我的大大大大的";
+    cout << "我的大大大大的";
+
+    return 0;
+}
