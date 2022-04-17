e30c4eec188fa29670f047efd88031af2e78eb5b
Pan_Yujie <646836760@qq.com>
2022-04-17 19:55:46 +0800

    master

---
+++ q.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N=500;
+
+int n,m;
+string yi,di;
+map<string,int>army;
+map<string,string>dist;
+int g[N][N];
+map<string,string>
+int main()
+{
+
+    cin>>n>>m>>yi>>di;
+        
+    for(int i=0;i<n-1;i++){
+        int x;
+        string s;
+        cin>>s>>x;
+        army[s]=x;
+    }
+
+    for(int i=0;i<m;i++){
+        int num;
+        string x,y;
+
+    }
+
+
+    return 0;
+}
